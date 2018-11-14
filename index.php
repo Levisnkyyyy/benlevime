4145276d47dff575cac296c9f5931ce612ac59ca
Ben <benschool01@gmail.com>
2018-11-14 15:44:49 +0200

    14th November

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ben Levi | Fullstack Web Developer</title>
+	<meta charset="utf-8">
+	  <meta name="description" content="Ben Levi | Fullstack Web Developer">
+  <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+  <meta name="author" content="Ben Levi">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" href="css/uikit.min.css" />
+	<link rel="stylesheet" href="css/style.css?v=3413" />
+	<link rel="stylesheet" href="css/animate.css" />
+	<link rel="stylesheet" href="css/aos.css" />
+	<meta name="theme-color" content="#000000" />
+	<meta name="msapplication-navbutton-color" content="#000000">
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000000">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="js/uikit.js"></script>
+	<script src="js/uikit-icons.min.js"></script>
+	<script src="js/aos.js"></script>
+	<script src="js/components/parallax.js"></script>
+	<script src="js/TweenMax.min.js"></script>
+	
+
+<link href="https://fonts.googleapis.com/css?family=Montserrat|Muli|Lato:300,900|Source+Sans+Pro:300,400,70" rel="stylesheet">
+<style>
+
+</style>
+</head>
+
+<body>
+
+<!-- #2B3137; background-image: linear-gradient(to bottom right, #232526, #414345);-->
+
+
+
+	<div class="uk-section uk-position-relative" id="header" style="padding-top: 20px !important;   " uk-height-viewport="offset-top: true; ">
+			<object class="uk-position-absolute " type="image/svg+xml" data="src/test2.svg" style="right: 0; z-index: 1;bottom: 0; opacity: 1;">
+				No SVG to remove this line
+			  </object>
+		<!--div class="cloudsscreen uk-absolute" style="height: 100%; width: 100%;">
+		<object  class="uk-position-absolute clouds" type="image/svg+xml" data="src/clouds.svg" style="width: 120px; opacity: 0.4;">
+  Your browser does not support SVG
+</object>
+<object class="uk-position-absolute cloud" type="image/svg+xml" data="src/cloud.svg" style="width: 120px; opacity: 0.4;">
+  Your browser does not support SVG
+</object>
+<object class="uk-position-absolute sun" type="image/svg+xml" data="src/sun.svg" style="width: 120px; opacity: 0.4;">
+  Your browser does not support SVG
+</object>
+		</div-->
+		
+
+		<div class="uk-container" style="z-index: 2;">
+			<div>
+				<div class=" uk-position-relative">
+						<img src="img/logo.png" class="uk-position-absolute uk-display-inline-block" style="width: 30px; vertical-align: text-bottom; z-index: 13; background-color: white;" />
+						<span class="animated fadeInLeft uk-position-absolute uk-display-inline-block" style="font-family: 'Muli', sans-serif; font-size: 1.2rem; left: 38px; top: 22px; z-index: 12; animation-delay: 900ms;">en<strong>levi</strong></span>
+				</div>
+				<div class="uk-float-right uk-display-inline-block bl-button" style="z-index: 3;">
+					Say Hi
+				</div>
+			</div>
+			<div class="flexheader" style="display: flex; justify-content: space-between; position: absolute; z-index: 2;  ">
+			<div class="uk-margin-xlarge-top uk-text-center uk-text-left@s" style="width: 50vw;">
+				<h1>Hello! I'm Ben.<br /></h1>
+				<p>I'm a Freelance Web Designer & Web Developer based in Haifa, Israel. I have a serious passion to simple designs, and creating a unique user experience.</p>
+				<div class="bl-button-inv">CONTACT ME </div>
+			</div>
+			<div style="flex-grow: 2">
+				f
+			</div>
+		</div>
+			<div class="uk-position-bottom-center mice">
+				<svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 2c1.103 0 2 .897 2 2v12c0 3.309-2.691 6-6 6s-6-2.691-6-6v-12c0-1.103.897-2 2-2h8zm4 2c0-2.209-1.791-4-4-4h-8c-2.209 0-4 1.791-4 4v12c0 4.418 3.582 8 8 8s8-3.582 8-8v-12zm-7 1c0-.552-.447-1-1-1s-1 .448-1 1v4c0 .552.447 1 1 1s1-.448 1-1v-4z"/></svg>
+				
+			
+			</div>
+		</div>
+
+	</div>
+	<div class="uk-section about uk-position-relative" style=" background-color: #9b59b6 ">
+		<div class="uk-container">
+				<h1>ABOUT</h1>
+
+		</div>
+	
+	</div>
+	<div class="uk-section uk-position-relative uk-height-viewport">
+	
+	</div>
+	<!--div class="uk-section" style="height: 530px; margin-top: -50px;">
+		<div class="rect"></div>
+		<div class="uk-position-relative">
+		<h1 style="font-family: 'Montserrat', sans-serif;color: white; font-size: 3rem; line-height: 2.7rem;">ABOUT ME<br /></h1>
+	</div>
+	</div>
+	<div class="uk-section uk-height-viewport">
+		ddd
+	</div-->
+		<!--img src="bg8.jpg" class="uk-position-cover" data-uk-parallax="y: -200, viewport: 1" alt=""  uk-cover-->
+    <!--video autoplay loop muted playsinline uk-cover>
+        <source src="1.mp4" type="video/mp4">
+        testssssssssssssssssssssssssssssssssssssssss
+		</video-->
+		<script>
+		$( document ).ready(function() {
+			AOS.init();
+			TweenMax.to('.minute', 2, {rotation: -90, repeat:-1, yoyo:true});
+
+});
+</script>
+</body>
+</html>
